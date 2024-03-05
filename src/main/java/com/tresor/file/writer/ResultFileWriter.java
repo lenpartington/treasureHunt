4d893cbc9ad7a922d021fa04e87274fe8b41fa06
lenpartington <len.ochiai@gmail.com>
2024-03-05 21:10:53 +0100

    [EVOL] added result file writer

---
+++ src/main/java/com/tresor/file/writer/ResultFileWriter.java
@@ -0,0 +1,52 @@
+package com.tresor.file.writer;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class ResultFileWriter {
+	private String fileName = "carte.txt";
+
+	public void writeFile(List<String> lines) {
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = null;
+		BufferedWriter bfw = null;
+		FileWriter fw = null;
+		try {
+		
+			file = new File(classLoader.getResource(".").getFile() + fileName);
+			fw = new FileWriter(file);
+			bfw = new BufferedWriter(fw);
+
+			for (String line : lines) {
+
+				bfw.write(line);
+				bfw.newLine();
+
+			}
+			bfw.flush();
+			bfw.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bfw != null) {
+					bfw.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public ResultFileWriter(String fileName) {
+		super();
+		this.fileName = fileName;
+	}
+
+}
---
+++ src/test/java/com/tresor/file/reader/ResultFileWriterTest.java
@@ -0,0 +1,26 @@
+package com.tresor.file.reader;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.tresor.file.writer.ResultFileWriter;
+
+public class ResultFileWriterTest {
+
+	@Test
+	public void writeFile() {
+		final String fileNameResult = "carte_result.txt";
+		FileReader reader = new FileReader("carte_vide.txt");
+		List<String> data = reader.readFile();
+		ResultFileWriter writer = new ResultFileWriter(fileNameResult);
+		writer.writeFile(data);
+		reader = new FileReader(fileNameResult);
+		List<String> lines = reader.readFile();
+		assertEquals("#commentaire", lines.get(0));
+		assertEquals("C - 3 - 4", lines.get(1));
+
+	}
+}
