9a9f8376f24e267881c27d1a042b9c444f87de24
len.partington <len.partington@ausy.fr>
2024-03-11 17:14:14 +0100

    [TEST] test hash and equals on aventurier

--- src/test/java/com/tresor/application/domain/aventurier/AventurierCreationTest.java
+++ src/test/java/com/tresor/application/domain/aventurier/AventurierCreationTest.java
@@ -1,6 +1,9 @@
 package com.tresor.application.domain.aventurier;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -16,6 +19,26 @@ public class AventurierCreationTest {
 		Aventurier expected = new Aventurier("Lara",1,1,"S","AADADAGGA");
 		assertEquals(expected, result);
 		
+		
+	}
+	
+	
+	@Test
+	public void testAventurierEquals() {
+		String line = "A - Lara - 1 - 1 - S - AADADAGGA";
+		Aventurier result = new Aventurier(line);
+		Aventurier expected = new Aventurier("Lara",1,1,"S","AADADAGGA");
+		assertTrue(expected.equals(result));
+		assertEquals(expected.hashCode(),result.hashCode());
+		
+	}
+	@Test
+	public void testAventurierNotEquals() {
+		String line = "A - Lara - 1 - 1 - S - AADADAGGA";
+		Aventurier result = new Aventurier(line);
+		Aventurier expected = new Aventurier("Lara",1,2,"S","AADADAGGA");
+		assertFalse(expected.equals(result));
+		assertNotEquals(expected.hashCode(),result.hashCode());
 		
 	}
 }
