98803168d0068c0fd6fcb2eca351b44874ec494d
len.partington <len.partington@ausy.fr>
2024-03-11 17:15:36 +0100

    [CLEAN] cleaning up test

--- src/test/java/com/tresor/application/domain/aventurier/AventurierCreationTest.java
+++ src/test/java/com/tresor/application/domain/aventurier/AventurierCreationTest.java
@@ -7,38 +7,31 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import com.tresor.application.domain.aventurier.Aventurier;
-
 public class AventurierCreationTest {
 
-	
 	@Test
 	public void createAventurier() {
 		String line = "A - Lara - 1 - 1 - S - AADADAGGA";
 		Aventurier result = new Aventurier(line);
-		Aventurier expected = new Aventurier("Lara",1,1,"S","AADADAGGA");
+		Aventurier expected = new Aventurier("Lara", 1, 1, "S", "AADADAGGA");
 		assertEquals(expected, result);
-		
-		
 	}
-	
-	
+
 	@Test
 	public void testAventurierEquals() {
 		String line = "A - Lara - 1 - 1 - S - AADADAGGA";
 		Aventurier result = new Aventurier(line);
-		Aventurier expected = new Aventurier("Lara",1,1,"S","AADADAGGA");
+		Aventurier expected = new Aventurier("Lara", 1, 1, "S", "AADADAGGA");
 		assertTrue(expected.equals(result));
-		assertEquals(expected.hashCode(),result.hashCode());
-		
+		assertEquals(expected.hashCode(), result.hashCode());
 	}
+
 	@Test
 	public void testAventurierNotEquals() {
 		String line = "A - Lara - 1 - 1 - S - AADADAGGA";
 		Aventurier result = new Aventurier(line);
-		Aventurier expected = new Aventurier("Lara",1,2,"S","AADADAGGA");
+		Aventurier expected = new Aventurier("Lara", 1, 2, "S", "AADADAGGA");
 		assertFalse(expected.equals(result));
-		assertNotEquals(expected.hashCode(),result.hashCode());
-		
+		assertNotEquals(expected.hashCode(), result.hashCode());
 	}
 }
