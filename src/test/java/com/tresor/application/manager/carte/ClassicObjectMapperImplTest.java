7f1b604cef4a2c74c9df728938b7a8b76e4724b7
len.partington <len.partington@ausy.fr>
2024-03-11 17:07:30 +0100

    [CLEAN] removed unsued import

--- src/test/java/com/tresor/application/manager/carte/ClassicObjectMapperImplTest.java
+++ src/test/java/com/tresor/application/manager/carte/ClassicObjectMapperImplTest.java
@@ -2,7 +2,6 @@ package com.tresor.application.manager.carte;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import java.nio.file.Paths;
 import java.util.List;
